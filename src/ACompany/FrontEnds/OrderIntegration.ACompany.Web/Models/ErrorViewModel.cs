8c2346d71f534e982b680629ed63085901f20a6f
ozberkesn <94966406+ozberkesn@users.noreply.github.com>
2024-05-17 13:31:58 +0300

    A Company WebUI added

---
+++ src/ACompany/FrontEnds/OrderIntegration.ACompany.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OrderIntegration.ACompany.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
